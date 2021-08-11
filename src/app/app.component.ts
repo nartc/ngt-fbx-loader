bf1f133f82cab606207cf85066d70e818ecb1ec8
Chau Tran <ctran@Chaus-MacBook-Pro.local>
2021-08-11 16:51:57 -0500

    finish

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,32 +1,143 @@
-import { Component } from '@angular/core';
+import {
+  AnimationStore,
+  CanvasStore,
+  LoaderService,
+} from "@angular-three/core";
+import {
+  ChangeDetectionStrategy,
+  Component,
+  NgZone,
+  OnInit,
+} from "@angular/core";
+import { filter, switchMap, take, tap, withLatestFrom } from "rxjs/operators";
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import { FBXLoader } from "three/examples/jsm/loaders/FBXLoader";
 
 @Component({
-  selector: 'app-root',
+  selector: "app-root",
   template: `
-    <!--The content below is only a placeholder and can be replaced.-->
-    <div style="text-align:center" class="content">
-      <h1>
-        Welcome to {{title}}!
-      </h1>
-      <span style="display: block">{{ title }} app is running!</span>
-      <img width="300" alt="Angular Logo" src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAyNTAgMjUwIj4KICAgIDxwYXRoIGZpbGw9IiNERDAwMzEiIGQ9Ik0xMjUgMzBMMzEuOSA2My4ybDE0LjIgMTIzLjFMMTI1IDIzMGw3OC45LTQzLjcgMTQuMi0xMjMuMXoiIC8+CiAgICA8cGF0aCBmaWxsPSIjQzMwMDJGIiBkPSJNMTI1IDMwdjIyLjItLjFWMjMwbDc4LjktNDMuNyAxNC4yLTEyMy4xTDEyNSAzMHoiIC8+CiAgICA8cGF0aCAgZmlsbD0iI0ZGRkZGRiIgZD0iTTEyNSA1Mi4xTDY2LjggMTgyLjZoMjEuN2wxMS43LTI5LjJoNDkuNGwxMS43IDI5LjJIMTgzTDEyNSA1Mi4xem0xNyA4My4zaC0zNGwxNy00MC45IDE3IDQwLjl6IiAvPgogIDwvc3ZnPg==">
-    </div>
-    <h2>Here are some links to help you start: </h2>
-    <ul>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://angular.io/tutorial">Tour of Heroes</a></h2>
-      </li>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://angular.io/cli">CLI Documentation</a></h2>
-      </li>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://blog.angular.io/">Angular blog</a></h2>
-      </li>
-    </ul>
-    
+    <ngt-canvas
+      [camera]="{ fov: 45, near: 1, far: 2000, position: [100, 200, 300] }"
+      [scene]="{
+        background: ['#a0a0a0'] | color,
+        fog: ['#a0a0a0', 200, 1000] | fog
+      }"
+      [shadows]="true"
+      [linear]="true"
+    >
+      <ngt-stats></ngt-stats>
+      <app-scene></app-scene>
+    </ngt-canvas>
   `,
-  styles: []
+  styles: [],
 })
 export class AppComponent {
-  title = 'ngt-fbx-loader';
+  title = "ngt-fbx-loader";
+}
+
+const clock = new THREE.Clock();
+
+@Component({
+  selector: "app-scene",
+  template: `
+    <ngt-orbit-controls
+      (ready)="onOrbitControlsReady($event)"
+    ></ngt-orbit-controls>
+
+    <ngt-hemisphere-light
+      o3d
+      [args]="['#ffffff', '#444444']"
+      [position]="[0, 200, 0]"
+    ></ngt-hemisphere-light>
+
+    <ngt-directional-light
+      o3d
+      [args]="['#ffffff']"
+      [position]="[0, 200, 100]"
+      [castShadow]="true"
+      (ready)="onDirectionalLightReady($event)"
+    ></ngt-directional-light>
+
+    <ngt-mesh
+      o3d
+      [rotation]="[-(1 | mathConst: 'PI') / 2, 0, 0]"
+      [receiveShadow]="true"
+    >
+      <ngt-plane-geometry [args]="[2000, 2000]"></ngt-plane-geometry>
+      <ngt-mesh-phong-material
+        [parameters]="{ color: '#999999', depthWrite: false }"
+      ></ngt-mesh-phong-material>
+    </ngt-mesh>
+
+    <ngt-grid-helper
+      o3d
+      [args]="[2000, 20, '#000000', '#000000']"
+      (ready)="onGridHelperReady($event)"
+    ></ngt-grid-helper>
+  `,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class SceneComponent implements OnInit {
+  mixer?: THREE.AnimationMixer;
+
+  constructor(
+    private loaderService: LoaderService,
+    private ngZone: NgZone,
+    private canvasStore: CanvasStore,
+    private animationStore: AnimationStore
+  ) {}
+
+  ngOnInit() {
+    this.ngZone.runOutsideAngular(() => {
+      this.canvasStore.active$
+        .pipe(
+          filter((active) => active),
+          switchMap(() =>
+            this.loaderService.use(FBXLoader, "assets/Samba-Dancing.fbx")
+          ),
+          withLatestFrom(this.canvasStore.scene$),
+          tap(([group, scene]) => {
+            this.mixer = new THREE.AnimationMixer(group);
+            this.mixer.clipAction(group.animations[0]).play();
+            group.traverse((child) => {
+              if ((child as THREE.Mesh).isMesh) {
+                child.castShadow = true;
+                child.receiveShadow = true;
+              }
+            });
+
+            this.animationStore.registerAnimation(() => {
+              const delta = clock.getDelta();
+              if (this.mixer) {
+                this.mixer.update(delta);
+              }
+            });
+
+            if (scene) {
+              scene.add(group);
+            }
+          }),
+          take(1)
+        )
+        .subscribe();
+    });
+  }
+
+  onDirectionalLightReady(dirLight: THREE.DirectionalLight) {
+    dirLight.shadow.camera.top = 180;
+    dirLight.shadow.camera.bottom = -100;
+    dirLight.shadow.camera.left = -120;
+    dirLight.shadow.camera.right = 120;
+  }
+
+  onGridHelperReady(gridHelper: THREE.GridHelper) {
+    (gridHelper.material as THREE.Material).opacity = 0.2;
+    (gridHelper.material as THREE.Material).transparent = true;
+  }
+
+  onOrbitControlsReady(orbitControls: OrbitControls) {
+    orbitControls.target.set(0, 100, 0);
+    orbitControls.update();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,42 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { ThreeOrbitControlsModule } from "@angular-three/controls/orbit-controls";
+import {
+  ThreeColorPipeModule,
+  ThreeCoreModule,
+  ThreeFogPipeModule,
+  ThreeMathConstantPipeModule,
+} from "@angular-three/core";
+import { ThreePlaneBufferGeometryModule } from "@angular-three/core/geometries";
+import { ThreeGridHelperModule } from "@angular-three/core/helpers";
+import {
+  ThreeDirectionalLightModule,
+  ThreeHemisphereLightModule,
+} from "@angular-three/core/lights";
+import { ThreeMeshPhongMaterialModule } from "@angular-three/core/materials";
+import { ThreeMeshModule } from "@angular-three/core/meshes";
+import { ThreeStatsModule } from "@angular-three/core/stats";
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
 
-import { AppComponent } from './app.component';
+import { AppComponent, SceneComponent } from "./app.component";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, SceneComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ThreeCoreModule,
+    ThreeColorPipeModule,
+    ThreeFogPipeModule,
+    ThreeHemisphereLightModule,
+    ThreeDirectionalLightModule,
+    ThreeMeshModule,
+    ThreeMathConstantPipeModule,
+    ThreePlaneBufferGeometryModule,
+    ThreeMeshPhongMaterialModule,
+    ThreeGridHelperModule,
+    ThreeOrbitControlsModule,
+    ThreeStatsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
